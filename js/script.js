fabbcc9f4893c9bb285a04e566424f9e877a803f
Евгений Михайлищев <88135196+Mikhaylishchev@users.noreply.github.com>
2022-05-27 10:34:24 +0500

    tabs

---
+++ js/script.js
@@ -0,0 +1,38 @@
+'use-strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'); //      Tabs
+    const tabsContent = document.querySelectorAll('.tabcontent');
+    const tabClassActive = 'tabheader__item_active';
+
+    function hideTabsContent() {
+
+        tabsContent.forEach(item => {
+
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(tab => tab.classList.remove(tabClassActive));
+    }
+
+    function showTabContent(i = 0) {
+
+        tabsContent[i].classList.add('show', 'fade');
+        tabs[i].classList.add(tabClassActive);
+    }
+
+    tabs.forEach((item, i) => {
+
+        item.addEventListener('click', (event) => {
+
+            hideTabsContent();
+            showTabContent(i);
+        })
+    });
+
+    hideTabsContent();
+    showTabContent();
+git
+})
